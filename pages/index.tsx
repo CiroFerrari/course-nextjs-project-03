7b4821a1bbea3eff9a02b077d838c05dbdc763a1
Franco Ciro Ferrari <francociroferrari@gmail.com>
2023-01-25 13:54:33 -0300

    EntryList and EntryCard components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Layout } from "@/components/layouts";
+import { EntryList } from "@/components/ui";
 import { Card, CardContent, CardHeader, Grid } from "@mui/material";
 import { NextPage } from "next";
 
@@ -9,20 +10,21 @@ const HomePage: NextPage = () => {
         <Grid item xs={12} sm={4}>
           <Card sx={{ height: 'calc(100vh - 100px)' }}>
             <CardHeader title="Pendientes" />
-            <CardContent>
-              {/* Agregar una nueva entrada */}
-              {/* Listado de las entradas */}
-            </CardContent>
+            {/* Agregar una nueva entrada */}
+            {/* Listado de las entradas */}
+            <EntryList />
           </Card>
         </Grid>
         <Grid item xs={12} sm={4}>
           <Card sx={{ height: 'calc(100vh - 100px)' }}>
             <CardHeader title="En Progreso" />
+            <EntryList />
           </Card>
         </Grid>
         <Grid item xs={12} sm={4}>
           <Card sx={{ height: 'calc(100vh - 100px)' }}>
             <CardHeader title="Completadas" />
+            <EntryList />
           </Card>
         </Grid>
       </Grid>
