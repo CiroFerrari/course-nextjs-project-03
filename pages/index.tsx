ae3d23e151e671d8ace053c5e37c7e3d3f41f93c
Franco Ciro Ferrari <francociroferrari@gmail.com>
2023-01-25 11:05:48 -0300

    Home design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,27 @@
 import { Layout } from "@/components/layouts";
-import { Typography } from "@mui/material";
+import { Card, CardHeader, Grid } from "@mui/material";
 import { NextPage } from "next";
 
 const HomePage: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant='h1' color='primary'>Hola Mundo</Typography>
+    <Layout title='Home - OpenJira'>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="Pendientes" />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="En Progreso" />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="Completadas" />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
