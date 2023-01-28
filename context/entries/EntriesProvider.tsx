939267e8f398a1d4c679d753e75f0b9dec3601ee
Franco Ciro Ferrari <francociroferrari@gmail.com>
2023-01-28 12:31:23 -0300

    POST new entry from frontend

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,7 +1,6 @@
 import { entriesApi } from "@/apis";
 import { Entry } from "@/interfaces";
 import { FC, ReactNode, useEffect, useReducer } from "react";
-import { v4 as uuidv4 } from 'uuid';
 import { EntriesContext, entriesReducer } from "./";
 
 export interface EntriesState {
@@ -16,15 +15,9 @@ export const EntriesProvider: FC<{ children: ReactNode }> = ({ children }) => {
 
   const [state, dispatch] = useReducer(entriesReducer, ENTRIES_INITIAL_STATE);
 
-  const addNewEntry = (description: string) => {
-    const newEntry: Entry = {
-      _id: uuidv4(),
-      description,
-      createdAt: Date.now(),
-      status: "pending"
-    }
-
-    dispatch({ type: '[Entry] Add-Entry', payload: newEntry })
+  const addNewEntry = async (description: string) => {
+    const { data } = await entriesApi.post<Entry>('/entries', { description });
+    dispatch({ type: '[Entry] Add-Entry', payload: data });
   }
 
   const updateEntry = (entry: Entry) => {
